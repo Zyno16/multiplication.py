98320e55bde6ae7fb6ba216f2f239dccc290ec95
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-03-10 10:10:00 +0000

    Add files via upload

---
+++ aptitude.py
@@ -0,0 +1,26 @@
+def multiplication():
+    n = int(input("enter a number"))
+    while n!=0:
+        for i in range(1,11):
+            print(i,"* ",n," =",i*n)
+        n = int(input("enter a number(or 0 to quit)"))
+
+multiplication()
+    
+
+
+    
+    
+
+    
+
+       
+
+
+        
+    
+
+
+
+ 
+
